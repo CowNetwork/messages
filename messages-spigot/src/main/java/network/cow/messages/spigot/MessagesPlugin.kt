17f53e3a5fe8f93adde594e4a3e911fcdc3f100e
Benedikt Wüller <wuelleronline@gmail.com>
2021-05-02 04:09:51 +0200

    Revert "Try something"
    This reverts commit 22225c45b8e7bb72ce6e0c05fb3c3d32d1bc795c.

--- messages-spigot/src/main/java/network/cow/messages/spigot/MessagesPlugin.kt
+++ messages-spigot/src/main/java/network/cow/messages/spigot/MessagesPlugin.kt
@@ -1,5 +1,7 @@
 package network.cow.messages.spigot
 
+import dev.benedikt.localize.LocalizeService
+import dev.benedikt.localize.json.JsonHttpLocaleProvider
 import net.kyori.adventure.text.Component
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -10,19 +12,24 @@ class MessagesPlugin : JavaPlugin() {
 
     companion object {
         var PREFIX: Component? = null
+
+        @JvmStatic
+        fun setPrefix(prefix: Component?) {
+            PREFIX = prefix
+        }
     }
 
     override fun onEnable() {
-//        val translations = this.config.getConfigurationSection("translations")
-//
-//        val locales = translations?.getConfigurationSection("locales")
-//        locales?.getKeys(false)?.forEach { locale ->
-//            val section = locales.getConfigurationSection(locale) ?: return@forEach
-//            LocalizeService.provideLocale(locale, JsonHttpLocaleProvider(*section.getStringList("paths").toTypedArray()))
-//            LocalizeService.setCoreLocale(locale, section.getBoolean("core", false))
-//        }
-//
-//        LocalizeService.fallbackLocale = translations?.getString("fallbackLocale") ?: "en_US"
+        val translations = this.config.getConfigurationSection("translations")
+
+        val locales = translations?.getConfigurationSection("locales")
+        locales?.getKeys(false)?.forEach { locale ->
+            val section = locales.getConfigurationSection(locale) ?: return@forEach
+            LocalizeService.provideLocale(locale, JsonHttpLocaleProvider(*section.getStringList("paths").toTypedArray()))
+            LocalizeService.setCoreLocale(locale, section.getBoolean("core", false))
+        }
+
+        LocalizeService.fallbackLocale = translations?.getString("fallbackLocale") ?: "en_US"
     }
 
 }
